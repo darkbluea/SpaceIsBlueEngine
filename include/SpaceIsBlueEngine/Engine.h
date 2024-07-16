41095cb25e4f652f2d7b3e81bd4067267bae6faa
BlueaProjects <blueavideo@gmail.com>
2024-07-16 03:24:15 +0200

    first push

---
+++ include/SpaceIsBlueEngine/Engine.h
@@ -0,0 +1,22 @@
+#ifndef Engine_Engine_h
+#define Engine_Engine_h
+
+namespace SpaceIsBlueEngine
+{
+    namespace Engine
+    {
+        class Engine
+        {
+        public:
+            Engine()
+            {
+            }
+
+            ~Engine() {}
+
+            int Run();
+        };
+    }
+}
+
+#endif
---
+++ src/SpaceIsBlueEngine/Engine.cpp
@@ -0,0 +1,6 @@
+#include <SpaceIsBlueEngine/Engine.h>
+
+int SpaceIsBlueEngine::Engine::Engine::Run()
+{
+    return 0;
+}
